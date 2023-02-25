b28f54318f1fd734ed9b4e957bd7cc9d705fe206
Alfonso Nicholas Vieyra <anvieyra@circinus-48.ics.uci.edu>
2023-02-24 22:10:00 -0800

    added to query and modded inverted index file

--- inverted_index/inverted_index.py
+++ inverted_index/inverted_index.py
@@ -15,14 +15,13 @@ class InvertedIndex:
         self.docID: int = 0
         self.stopWords = helpers.stopWords()
         self.urlDict = {}
-        self.invertedIndex = {} 
+        self.path: dict = {}
 
     def readFile(self, path):
         """
         :param path: string - file paths to open from directory
         :return soup.get_text(): string - content left from removing the html
         """
-
         with open(path, 'r') as f:
             data = json.load(f)
             content = data['content']
@@ -45,53 +44,120 @@ class InvertedIndex:
 
         return wordList
 
-    def wordFrequencies(self, textList: list)->dict:
-        words = {}
-        for word in textList:
-            if word not in words:
-                words[word] = postings.Postings()
-                words[word] = posting
+    def wordPostings(self, textList: list)->dict:
+        words: dict = {}
+        for i in range(len(textList)):
+            if textList[i] not in words:
+                words[textList[i]] = [postings.Postings()]
+                words[textList[i]][0].frequs = 1
+                words[textList[i]][0].docID = self.docID
             else:
-                words[word] += 1
-        
-        for pos in range(textList):
-            words[textList[pos]]
+                words[textList[i]][0].frequs += 1
+            words[textList[i]][0].pos.append(i)
 
-    def buildIndex(self, textList, url):
+        self.calculateTF(words, len(textList))
+        return words
+
+    def buildIndex(self, wordposts: dict, url: str):
         """
         creates an inverted index of the document that is passed in and updates the document id index(URL dict)
         :param textList: content of the passed in document
         :return: dict - dictionary of words and the amount of times they appear in a document
         """
-
-        docIndex = {} #initialize a dictionary for the document
-        countDict = Counter(textList) #get a dictionary in the format of {token:appearances in the doc}
-        docID = f"DocID: {self.docID}"
-
-        #create the inverted index for the document
-        for key, value in countDict.items():
-            docIndex[key] = [(docID, value)]
+        for word in wordposts:
+            path = self.path[word[0]]
+            if os.path.isfile(path) == False:
+                with open(path, "wb") as f:
+                    index: dict = {}
+                    index[word] = wordposts[word]
+                    pickle.dump(index, f)
+                    f.close()
+            else:
+                with open(path, "rb+") as f:
+                    index: dict = pickle.load(f)
+                    self.updateFile(word, wordposts, index)
+                    f.seek(0)
+                    pickle.dump(index, f)
+                f.close()
 
         #keep track of the url with the document ID
-        self.urlDict[docID] = url
+        self.urlDict[self.docID] = url
         self.docID += 1
 
-        return docIndex
-
-    def updateIndex(self, docIndex):
+    def updateFile(self, word: str, src: dict, dest: dict)->None:
         """
-        updates the inverted index
-        :param docIndex:
+        updateFile updates the dest param with info contained in src param
+        param: src key, src dict, dest dict
+        Void function
+        """
+        if word not in dest:
+            dest[word] = src[word]
+        else:
+            dest[word].append(src[word][0])
+
+    def calculateTF(self, wordsDict: dict, total_doc_tokens)->None:
+        """
+        Calculate TF score
+        param: dict, int 
         :return:
         """
-        #traverse the document index
-        for key, value in docIndex.items():
-            #if the token is found in the Inverted index then append to the keys values
-            if key in self.invertedIndex:
-                self.invertedIndex[key].append(value)
-            #if token isnt found -> create new key in inverted index
-            if key not in self.invertedIndex:
-                self.invertedIndex[key] = value
+        for key in wordsDict:
+            frequencies = wordsDict[key][0].frequs
+            wordsDict[key][0].tf = (frequencies / total_doc_tokens)
+    
+    def calculateIDF(self)->None:
+        """
+        calculateIDF calculates the idf score for each term in the
+        posting. It gets deployed after the index is built and docID
+        is the value of the last document searched which repesents the
+        nth document in the index. 
+        param: None
+        return: None
+        """
+        total_docs = self.docID #docID will be the nth doc
+        for i in range(26): # total letters in alphabet
+            letter = chr(ord('a') + i)
+            path = self.path[letter]
+            # check if path exists first
+            if os.path.isfile(path):
+                with open(path, "rb") as f:
+                    letter_index = pickle.load(f)
+                    # each word is the key in the partial index
+                    for word in letter_index:
+                        total_word_app = len(letter_index[word]) # total appearance in n docs
+                        idf_score = log(total_docs / total_word_app)
+                        # each post is Posting() struct that will have the new idf score
+                        for post in letter_index[word]:
+                            post.idf = idf_score
+                    f.close()
+        
+        for i in range(10):
+            path = self.path[i]
+
+            if os.path.isfile(path):
+                with open(path, "rb") as f:
+                    numbers_index = pickle.load(f)
+                    
+                    for num in numbers_index:
+                        total_num_apps = len(numbers_index[num])
+                        idf_score = log(total_docs / total_num_apps)
+
+                        for post in numbers_index[num]:
+                            post.idf = idf_score
+                    f.close()
+
+    def alphaToPath(self)->None:
+        """
+        Func: modifies the self.path dict so that it maps lowers case
+        letters to a file path contain terms that start with the key.
+        param: None
+        return: None
+        """
+        for i in range(26):
+            letter = str(chr(ord('a') + i))
+            self.path[letter] = "./pickle/" + letter + ".pickle"
+            if i < 10:
+                self.path[str(i)] = "./pickle/numbers.pickle"
 
     def store(self, content, fileName):
         """
@@ -102,20 +168,6 @@ class InvertedIndex:
         with open(fileName, 'wb') as file:
             pickle.dump(content,file, protocol= pickle.HIGHEST_PROTOCOL)
 
-    def calculateTF(self): #implement later
-        """
-        Calculate TF score
-        :return:
-        """
-        pass
-
-    def getinvertedIndex(self):
-        """
-        Getter function for the inverted index
-        :return: dict
-        """
-        return self.invertedIndex
-
     def docIndex(self):
         """
         Getter function for document indexes
@@ -123,29 +175,40 @@ class InvertedIndex:
         """
         return self.urlDict
 
+    def clearIndex(self)->None:
+        alpha = {}
+        for i in range(26):
+            letter = chr(ord('a') + i)
+            alpha[letter] = "./pickle/" + letter + ".pickle"
+
+        for key in alpha:
+            clear: dict = {}
+            with open(alpha[key], "wb") as f:
+                pickle.dump(clear, f)
+                f.close()
+            print(os.path.getsize(alpha[key]))
+
     def Run(self):
         """
         The return method handles the main logic of this program
         :return: None
         """
         #assuming the DEV folder is in the same directory as the code
-        path = r"DEV"
+        path = r"/home/anvieyra/cs121/space-time-crawler/index/DEV"
+
+        self.alphaToPath() #creates mapping to pickle files
         #traverse the files in the DEV directory
         for subdir, dir, files in os.walk(path):
             for f in files:
                 file = os.path.join(subdir, f) #create file path
                 text, url = self.readFile(file) #read the file
-                processedTextList = self.processText(text) #process the text
-                docIndex = self.buildIndex(processedTextList, url) #build the inverted index for the single document
-                self.updateIndex(docIndex) #update the actual Inverted Index
+                processedTextList = self.processText(text) #process the text into a word list
+                wordposts = self.wordPostings(processedTextList)
+                docIndex = self.buildIndex(wordposts, url) #build the inverted index for the single document
                 print(f"{self.docID}:{url}")
             #store content that was retrieved
-            self.store(self.invertedIndex, "inverted_index.pickle")
-            self.store(self.urlDict,"url_index.pickle")
-
-        print(f"Report:\n"
-              f"Word Count: {len(self.invertedIndex.keys())}\n"
-              f"Unique Pages: {self.docID}")
+            self.store(self.urlDict,"./pickle/url_index.pickle")
+        self.calculateIDF()
 
 if __name__ == '__main__':
     inverted_index = InvertedIndex()
--- inverted_index/postings.py
+++ inverted_index/postings.py
@@ -1,10 +1,15 @@
+import struct
+import pickle
+import os
+import inverted_index
 class Postings:
     def __init__(self)-> None:
-        self.term: str = ""
+        #self.term: str = ""
         self.docID: int = 0
         self.frequs: int = 0
         self.pos: list = []
         self.tf: float = 0
+        self.idf: float = 0
 
     # We will calculate the TF (term Frequencies) in this function by
     # by dividing the total number of appearences by the total number
@@ -17,7 +22,7 @@ class Postings:
         self.pos.append(position)
 
     def printPosting(self, post)->None:
-        print(post.term + "\ndocID: " + str(post.docID) + "\tfrequencies: " + str(post.frequs) 
+        print("\ndocID: " + str(post.docID) + "\tfrequencies: " + str(post.frequs) 
             + f"\npositions: {post.pos}" + "\tTF: " + f"{post.tf:.2f}")
 
     def Run(self):
@@ -25,18 +30,15 @@ class Postings:
         textList = ['the', 'in', 'the', 'be', 'to', 'from', 'to']
         for i in range(len(textList)):
             if textList[i] not in index:
-                index[textList[i]] = Postings()
-                index[textList[i]].frequs = 1
+                index[textList[i]] = [Postings()]
+                index[textList[i]][0].frequs = 1
             else:
-                index[textList[i]].frequs += 1
-            index[textList[i]].pos.append(i)
-            index[textList[i]].term = textList[i]
+                index[textList[i]][0].frequs += 1
+            index[textList[i]][0].pos.append(i)
         
         for key in index: 
-            index[key].tf = index[key].frequs / len(textList)
+            index[key][0].tf = index[key][0].frequs / len(textList)
 
-        for key in index:
-            postings.printPosting(index[key])
 
 if __name__ == '__main__':
     postings = Postings()
